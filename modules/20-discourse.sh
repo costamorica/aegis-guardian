b381effeca2b6b32b20e274a5ddd86efd2a3538f
costamorica <72514370+costamorica@users.noreply.github.com>
2026-07-25 19:16:51 +0200

    Delete modules directory

--- modules/20-discourse.sh
+++
@@ -1,8 +0,0 @@
-module_name() {
-    printf 'Discourse'
-}
-
-module_check() {
-    check_container "discourse.container" "$DISCOURSE_CONTAINER"
-    check_http_local "discourse.http" "$FORUM_URL" "$FORUM_DOMAIN"
-}
--- modules/40-site.sh
+++
@@ -1,7 +0,0 @@
-module_name() {
-    printf 'Site web'
-}
-
-module_check() {
-    check_http_local "website.http" "$SITE_URL" "$SITE_DOMAIN"
-}
